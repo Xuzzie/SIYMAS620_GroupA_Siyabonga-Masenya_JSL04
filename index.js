37c7550385f3f48aa912ebc2672b366100e96092
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-07-19 11:29:15 +0200

    Realized I can combine template literals with ternary options and therefore using this as an opportunity to practice these two concepts together

--- index.js
+++ index.js
@@ -6,29 +6,34 @@
 
 let userPreference = "Sugar-free"; // Possible values: 'Regular', 'Sugar-free', or undefined
 
-let flavor;
+//let flavor;
 
-if (userPreference) {
-  flavor = userPreference;
-} else {
-  flavor = "regualar";
-}
+//if (userPreference) {
+// flavor = userPreference;
+//} else {
+//flavor = "Regualar";
+//}
 
-console.log(``);
+let flavor = userPreference ? userPreference : "regular";
+console.log(flavor);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge.
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+letinfomsg = cansLeft`${cansLeft < 5 ? "Time to restock!" : "We're stocked!"}`;
+console.log(letinfomsg);
 
 // #### Challenge 3: Workout Intensity
 // Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout.
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+let currentheartrate = `${
+  heartRate < 100 ? "Boost needed!" : "Energy levels are high!"
+}`;
+console.log(currentheartrate);
 
 // #### Challenge 4: Temperature Suitability
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!"
