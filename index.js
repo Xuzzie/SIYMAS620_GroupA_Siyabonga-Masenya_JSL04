13bfe857dfdb8807b02337adaf105f2973c9b2d4
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-07-19 11:41:06 +0200

    Used the  less or equal to operator to find the  answer to this question whilst using the same framework that i used in previous questions

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ console.log(currentheartrate);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+let tempstatus = `${
+  currentTemp <= 5 ? "Needs a cooler!" : "Chilled to perfection!"
+}`;
+console.log(tempstatus);
 
 // #### Challenge 5: Late Night Coding Session
 // Determine if it's a good idea to have a Monster Energy drink based on the current hour.
